55881ee4086e552fa3bb516d464027d8f7135d4f
DuanLeeWatkins <leeduan19@gmail.com>
2021-06-01 18:05:34 -0400

    updated files

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,7 @@ const User = require("../models/User");
 
 module.exports = function (passport) {
   passport.use(
+    
     new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
       User.findOne({ email: email.toLowerCase() }, (err, user) => {
         if (err) {
